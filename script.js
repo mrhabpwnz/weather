3e7abcca01b0cf75df3878e86843b9b4ed7db9d6
Nikolay Syritsky <68183725+mrhabpwnz@users.noreply.github.com>
2021-11-10 19:56:18 +0300

    Signed-off-by: Nikolay Syritsky <68183725+mrhabpwnz@users.noreply.github.com>

--- script.js
+++ script.js
@@ -103,14 +103,14 @@ console.log(data);
 
     bigIcon.src = data.current.condition.icon;
     debugger
-    
+
     for(let i = 0; i < data.forecast.forecastday.length; i++) {
         weatherSmallIcon[i].src = data.forecast.forecastday[i].day.condition.icon;
     }
 
-    weatherSmallIcon[0].src = data.forecast.forecastday[0].day.condition.icon;
-    weatherSmallIcon[1].src = data.forecast.forecastday[1].day.condition.icon;
-    weatherSmallIcon[2].src = data.forecast.forecastday[2].day.condition.icon;
+    // weatherSmallIcon[0].src = data.forecast.forecastday[0].day.condition.icon;
+    // weatherSmallIcon[1].src = data.forecast.forecastday[1].day.condition.icon;
+    // weatherSmallIcon[2].src = data.forecast.forecastday[2].day.condition.icon;
 
 
     // for (let [classname, weatherArray] of Object.entries(classnamesToWeathermap)) {
